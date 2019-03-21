cb6dffe6421f324ce75440118e24e88f5e8b543b
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-03-21 18:12:17 +0900

    Update Paralysis.java

--- statuses/Paralysis.java
+++ statuses/Paralysis.java
@@ -3,10 +3,13 @@ import random.Random;
 import statuses.Status;
 
 public class Paralysis implements Status {
+    private byte turn = 3;
+    
     public boolean canMove() {
-        if(Random.random() <= 25) {
+        if(this.turn <= 0 || Random.random() <= 25) {
             return false;
         }
+        this.turn--;
         return true;
     }
 }
